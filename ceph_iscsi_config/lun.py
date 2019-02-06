1ee2416c9115143fbbb88c63817362c58bf0b427
Ricardo Marques <rimarques@suse.com>
2019-02-06 17:44:12 +0000

    Adds disk 'backstore_object_name'
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- ceph_iscsi_config/lun.py
+++ ceph_iscsi_config/lun.py
@@ -268,7 +268,8 @@ class LUN(GWObject):
         ]
     }
 
-    def __init__(self, logger, pool, image, size, allocating_host, backstore):
+    def __init__(self, logger, pool, image, size, allocating_host,
+                 backstore, backstore_object_name):
         self.logger = logger
         self.image = image
         self.pool = pool
@@ -280,6 +281,7 @@ class LUN(GWObject):
         # only uses shortname so it needs to be converted to shortname format
         self.allocating_host = allocating_host.split('.')[0]
         self.backstore = backstore
+        self.backstore_object_name = backstore_object_name
 
         self.error = False
         self.error_msg = ''
@@ -659,7 +661,8 @@ class LUN(GWObject):
                                  "allocating_host": self.allocating_host,
                                  "pool_id": rbd_image.pool_id,
                                  "controls": self.controls,
-                                 "backstore": self.backstore}
+                                 "backstore": self.backstore,
+                                 "backstore_object_name": self.backstore_object_name}
 
                     self.config.update_item('disks',
                                             self.config_key,
@@ -792,7 +795,7 @@ class LUN(GWObject):
 
             # First match on name, but then check the pool incase the same
             # name exists in multiple pools
-            if stg_object.name == self.config_key:
+            if stg_object.name == self.backstore_object_name:
 
                 found_it = True
                 break
@@ -849,7 +852,7 @@ class LUN(GWObject):
             if (settings.config.cephconf != '/etc/ceph/ceph.conf'):
                 cfgstring += ";conf={}".format(settings.config.cephconf)
 
-            new_lun = UserBackedStorageObject(name=self.config_key,
+            new_lun = UserBackedStorageObject(name=self.backstore_object_name,
                                               config=cfgstring,
                                               size=self.size_bytes,
                                               wwn=in_wwn, control=control_string)
@@ -882,7 +885,7 @@ class LUN(GWObject):
         # remove the device from all tpgs
         for t in lio_root.tpgs:
             for lun in t.luns:
-                if lun.storage_object.name == self.config_key:
+                if lun.storage_object.name == self.backstore_object_name:
                     try:
                         lun.delete()
                     except Exception as e:
@@ -894,7 +897,7 @@ class LUN(GWObject):
                         break       # continue to the next tpg
 
         for stg_object in lio_root.storage_objects:
-            if stg_object.name == self.config_key:
+            if stg_object.name == self.backstore_object_name:
                 try:
                     stg_object.delete()
                 except Exception as e:
@@ -1097,9 +1100,11 @@ class LUN(GWObject):
                                 with rbd.Image(ioctx, image_name) as rbd_image:
                                     RBDDev.rbd_lock_cleanup(logger, ips, rbd_image)
 
-                                    backstore = config.config['disks'][disk_key]['backstore']
-                                    lun = LUN(logger, pool, image_name,
-                                              rbd_image.size(), local_gw, backstore)
+                                    disk_config = config.config['disks'][disk_key]
+                                    backstore = disk_config['backstore']
+                                    backstore_object_name = disk_config['backstore_object_name']
+                                    lun = LUN(logger, pool, image_name, rbd_image.size(),
+                                              local_gw, backstore, backstore_object_name)
                                     if lun.error:
                                         raise CephiSCSIError("Error defining rbd "
                                                              "image {}".format(disk_key))
